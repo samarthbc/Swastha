f7aa4d0b1e2d769ea984abac433c612bc5fa4ad3
unknownalien1 <samarthbellam@gmail.com>
2024-02-14 15:58:34 +0530

    Changed the server side url to https://swastha.vercel.app

--- Frontend/swasthafr/src/context/UrlState.js
+++ Frontend/swasthafr/src/context/UrlState.js
@@ -3,7 +3,8 @@ import urlcontext from "./urlcontext";
 
 const UrlState = (props) =>{
     // let [server_url, setServer_url] = useState("http://localhost:5000")
-    let server_url = "http://localhost:5000"
+    // let server_url = "http://localhost:5000" // For local computer 
+    let server_url = "https://swastha.vercel.app"
 
     return(
         <urlcontext.Provider value={{server_url}}>
