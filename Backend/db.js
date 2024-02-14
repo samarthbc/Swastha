07f75f7708500f2e0c3a7a6f0785e32099580908
unknownalien1 <samarthbellam@gmail.com>
2024-02-14 13:53:17 +0530

    Updated databse connection to MongoDB atlas

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const mongoURL = "mongodb://localhost:27017/swastha?";
+// const mongoURL = "mongodb://localhost:27017/swastha?";
+const mongoURL = "mongodb+srv://unknownalien1:1210sampada@cluster0.fgxndej.mongodb.net/swastha?retryWrites=true&w=majority"; // MongoDB atlas Swastha
 // const mongoURL = "mongodb://192.168.0.106:27017/swastha?"; //IP address
 
 const connectToMongo = ()=>{
